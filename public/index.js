edbe2024e947ac0fcda346c9f8237400a09099f1
Filip Troníček <filip.tronicek@seznam.cz>
2021-01-22 07:31:06 +0000

    Make code more consistent

--- public/index.js
+++ public/index.js
@@ -1,16 +1,15 @@
 const NEXT_FAVICON = "favicon.next.png";
 const DEFAULT_FAVICON = "favicon.default.png";
 
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", () => {
   // Fetch the cookie value
   if (document.cookie.indexOf("gitlab_canary=true") >= 0) {
     document.querySelector("#next-switch").setAttribute("checked", true);
-
     setFavicon(NEXT_FAVICON);
   }
 
-  document.querySelector("#next-switch").addEventListener("change", function () {
-    document.cookie = "gitlab_canary="+ this.checked +";domain=.gitlab.com;path=/;expires=" + new Date(Date.now() + 31536000000).toUTCString();
+  document.querySelector("#next-switch").addEventListener("change", () => {
+    document.cookie = `gitlab_canary=${this.checked};domain=.gitlab.com;path=/;expires=${new Date(Date.now() + 31536000000).toUTCString()}`;
     const favicon = this.checked ? NEXT_FAVICON : DEFAULT_FAVICON;
     setFavicon(favicon);
   });
@@ -22,7 +21,7 @@ document.addEventListener("DOMContentLoaded", function () {
  * @param String faviconPath
  */
 function setFavicon(faviconPath) {
-  const faviconEl = document.getElementById("favicon");
+  const faviconEl = document.querySelector("#favicon");
   if (faviconEl && faviconPath) {
     faviconEl.setAttribute("href", faviconPath);
   }
